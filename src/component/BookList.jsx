3255283de24d1e0aff0a07e61492311edc108343
srikishore5727 <srikishore9080676683@gmail.com>
2023-12-23 11:52:31 +0530

    Kalvium Books

--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -25,7 +25,7 @@ const BookList = () => {
             .catch((err) => {
                 console.log(err);
             });
-    }, []);
+    },[]);
 
     const handleSearch = async () => {
         const headers = {
